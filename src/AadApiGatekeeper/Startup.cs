709098398907b71021ba9b055bb2035d7884a3ed
Martin Brandl <mbr@whiteduck.local>
2018-08-22 13:24:15 +0200

    Fixed missplaced semicolon

--- src/AadApiGatekeeper/Startup.cs
+++ src/AadApiGatekeeper/Startup.cs
@@ -19,7 +19,7 @@ namespace AadApiGatekeeper
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOptions();            ;
+            services.AddOptions();
 
             services.AddAuthProxy(options => Configuration.Bind("Proxy", options), options => Configuration.Bind("Authentication", options));                                   
 
